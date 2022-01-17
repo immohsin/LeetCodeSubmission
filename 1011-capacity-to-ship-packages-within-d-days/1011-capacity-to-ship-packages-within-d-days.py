b9137f358192b9bb14c5fa6119f7c9e7d74420af
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2022-01-17 14:09:10 +0530

    Time: 1195 ms (7.62%), Space: 17.3 MB (7.36%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,29 @@
+def isPossible(weights, days, mid):
+    a_day = 1
+    s = 0
+    for i in range(len(weights)):
+        if weights[i] > mid:
+            return False
+        s+=weights[i]
+        if s > mid:
+            a_day+= 1
+            s = weights[i]
+        if a_day > days:
+            return False
+    return True
+        
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        if len(weights) < days:
+            return -1
+        
+        start, end = min(weights), sum(weights)
+        res = 0
+        while start <= end:
+            mid = start + (end - start) // 2
+            if isPossible(weights, days, mid):
+                res = mid
+                end = mid - 1
+            else:
+                start = mid + 1
+        return res   

f2d6a314192eb20fa09ba1f255c132f86ec7dacf
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2022-01-20 20:11:36 +0530

    Time: 51 ms (34.71%), Space: 14.5 MB (27.15%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -3,12 +3,16 @@ class Solution:
         res = []
         nums.sort()
         def recur(arr, idx, out):
-            res.append(out.copy())
             for i in range(idx, len(nums)):
                 if i > idx and arr[i] == arr[i-1]:
                     continue
                 out.append(arr[i])
+                res.append(out.copy())
                 recur(arr, i+1, out)
                 out.pop()
         recur(nums, 0, [])
+        res.append([])
         return res
+
+#  [1], 
+# [], [1],

6ff75e009d5606b6075545661354d08ecd9f4edf
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2022-01-21 19:55:24 +0530

    Time: 73 ms (9.27%), Space: 14.2 MB (99.18%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        res = [[]]
         nums.sort()
         def recur(arr, idx, out):
             for i in range(idx, len(nums)):
@@ -11,8 +11,4 @@ class Solution:
                 recur(arr, i+1, out)
                 out.pop()
         recur(nums, 0, [])
-        res.append([])
         return res
-
-#  [1], 
-# [], [1],

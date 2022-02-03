9d8c452069f5311174836e2eab010682ddc840ec
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2022-02-03 16:07:16 +0530

    Time: 48 ms (95.47%), Space: 14 MB (92.69%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.py
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -1,13 +1,13 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
-        domains = {}
-
-        for pair in cpdomains:
-            countpair = pair.split(" ")
-            rep = int(countpair[0])
-            domain = countpair[1].split(".")
-            for i in range(len(domain)):
-                sub = '.'.join(domain[i:])
-                domains.setdefault(sub, []).append(rep)
-
-        return [f'{sum(j)} {i}' for i, j in domains.items()]
+        res = []
+        d = {}
+        for cpdomain in cpdomains:
+            split_1 = cpdomain.split(" ")
+            rep = int(split_1[0])
+            new = split_1[1].split('.')
+            for j in range(len(new)-1,-1,-1):
+                s = ".".join(new[j:])
+                d[s] = d.get(s, 0) + rep
+        res = [str(v) + " " + k for k,v in d.items()]
+        return res

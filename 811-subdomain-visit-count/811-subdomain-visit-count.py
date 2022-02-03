5b21b50d3b99866bde45a6f4ad956f5b07f07005
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2022-02-03 16:02:45 +0530

    Time: 88 ms (27.76%), Space: 13.8 MB (98.18%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.py
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -3,20 +3,12 @@ class Solution:
         res = []
         d = {}
         for cpdomain in cpdomains:
-            rep = ""
-            i = 0
-            n = len(cpdomain)
-            while i < n:
-                if cpdomain[i] >= '0' and cpdomain[i] <= '9':
-                    rep+=cpdomain[i]
-                else:
-                    break
-                i+=1
-            new = cpdomain[i+1:].split('.')
+            split_1 = cpdomain.split(" ")
+            rep = int(split_1[0])
+            new = split_1[1].split('.')
             for j in range(len(new)-1,-1,-1):
                 s = ".".join(new[j:])
-                d[s] = d.get(s, 0) + int(rep)
-
+                d[s] = d.get(s, 0) + rep
         for k,v in d.items():
             s = str(v) + " " + k
             res.append(s)

6c01e72c17c8070d9224e454d1beaf4c52b9303f
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2022-02-12 23:06:07 +0530

    Time: 36 ms (67.18%), Space: 13.9 MB (77.50%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,8 +1,4 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         n = int("".join(str(i) for i in digits)) + 1
-        res = []
-        while n:
-            res.append(n%10)
-            n = n//10
-        return reversed(res)
+        return [int(i) for i in str(n)]

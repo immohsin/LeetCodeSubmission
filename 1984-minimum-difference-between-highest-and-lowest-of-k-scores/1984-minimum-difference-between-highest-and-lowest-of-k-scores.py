0075897003aa3e9dadfa754425c1cb16ad7b824a
Mohsin Mumtaz <mohsinmumtaz21@gmail.com>
2022-02-20 20:20:28 +0530

    Time: 146 ms (51.03%), Space: 14.2 MB (79.47%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -4,13 +4,22 @@ class Solution:
         nums.sort()
         t = []
         i = 0
-        mmax, mmin = nums[0], nums[0]
         res = sys.maxsize
+        s = []
+        s1 = []
         for j in range(0, len(nums)):
+            while s and s[-1] < nums[j]:
+                s.pop()
+            s.append(nums[j])
+            while s and s[-1] > nums[j]:
+                s.pop()
+            s1.append(nums[j])
             t.append(nums[j])
             if j-i+1 == k:
-                res = min(res, max(t) - min(t))
-                if t[0] == nums[i]:
-                    t.pop(0)
+                res = min(res, s[0]-s1[0])
+                if s[0] == nums[i]:
+                    s.pop(0)
+                if s1[0] == nums[i]:
+                    s1.pop(0)
                 i+=1
         return res
